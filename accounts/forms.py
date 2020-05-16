65723c0500e497a50098a0576447ec746bf0aab2
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-16 18:02:20 +0530

    CRUD options

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from django import forms
+from .models import Order
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        widgets = {
+            'customer': forms.Select(attrs={'class': 'form-control'}),
+            'product': forms.Select(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+        }
