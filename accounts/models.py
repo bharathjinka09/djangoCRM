6d9acdc42765442066cb365002af787f5975e460
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-20 14:19:52 +0530

    Aded

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.EmailField(max_length=200, null=True)
-    profile_pic = models.ImageField(default='https://bharath-crm.herokuapp.com/static/images/logo.png', null=True, blank=True)
+    profile_pic = models.ImageField(default='profile1.png', null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
