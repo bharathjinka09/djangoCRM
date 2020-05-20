a2b0dcb77ca7b13bd495c7f56d3bd59d3e75fd70
bharathjinka09 <bharathjinka09@gmail.com>
2020-05-20 16:46:11 +0530

    Reset Password

--- crm/settings.py
+++ crm/settings.py
@@ -134,6 +134,13 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bharathjinka66@gmail.com'
+EMAIL_HOST_PASSWORD = 'kylpwstfouqfmvkt'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
